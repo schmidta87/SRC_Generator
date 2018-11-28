93b46812139830428099078c8c4f8b6215fbd294
Andrew Denniston <awild@mit.edu>
2018-11-28 10:42:52 -0500

    adding sigma cc2 to gen weight

--- gen_weight.cpp
+++ gen_weight.cpp
@@ -19,6 +19,7 @@ const double Xmin=1.;
 const double Xmax=2.;
 
 double sq(double x){ return x*x; };
+double dot4(double x0, TVector3 x, double y0, TVector3 y){return ((x0*y0)-(x*y))};
 double sigmaCC1(double Ebeam, TVector3 k, TVector3 p, bool isProton);
 
 int main(int argc, char ** argv)
@@ -243,6 +244,39 @@ double sigmaCC1(double Ebeam, TVector3 k, TVector3 p, bool isProton)
   double wS = p.Mag2() * sq(sin(p.Angle(q))) * (sq(F1) + QSqbar/(4.*mN*mN) * sq(kF2))/(E*Ebar);
   double wI = -p.Mag()*sin(p.Angle(q))*(Ebar + E)*(sq(F1) + QSqbar/(4.*mN*mN) * sq(kF2))/(E*Ebar);
 
+  double sigmaMott = cmSqGeVSq * 4. * sq(alpha) * k.Mag2() * sq(cos(k.Theta()/2.)) / sq(QSq);
+
+  double phi = q.Cross(k).Angle( q.Cross(p) );
+  return sigmaMott * ( sq(QSq)/q.Mag2() * wC +
+                       (QSq/(2.*q.Mag2()) + sq(tan(k.Theta()/2.))) * wT +
+                       QSq/q.Mag2() * sqrt(QSq/q.Mag2() + sq(tan(k.Theta()/2.))) * wI * cos(phi) +
+                       (QSq/q.Mag2() * sq(cos(phi)) + sq(tan(k.Theta()/2.))) * wS
+                       );
+}
+
+
+double sigmaCC2(double Ebeam, TVector3 k, TVector3 p, bool isProton)
+{
+  TVector3 q = TVector3(0.,0.,Ebeam) - k;
+  TVector3 pM = p-q;
+
+  double QSq = q.Mag2() - sq(Ebeam - k.Mag());
+  double E = sqrt(p.Mag2() + sq(mN));
+  double Ebar = sqrt(pM.Mag2() + sq(mN));
+  double omegabar = E-Ebar;
+  double QSqbar = q.Mag2() - sq(omegabar);
+
+  // Calculate form factors
+  double GE = (isProton)? Gdipole(QSq) : 1.91 * QSq * Gdipole(QSq) / (4.*sq(mN) + 5.6 * QSq);
+  double GM = (isProton)? 2.79*Gdipole(QSq) : -1.91*Gdipole(QSq);
+  double F1 = 0.5 * (GE + QSq*GM/(4.*sq(mN)));
+  double kF2 = (GM - GE)/(1. + QSq/(4.*sq(mN)));
+
+  double wC = (sq(E+Ebar)*(sq(F1) + QSqbar/(4.*mN*mN) * sq(kF2)) - q.Mag2()*sq(F1 + kF2))/(4.*E*Ebar);
+  double wT = QSqbar*sq(F1 + kF2)/(2.*Ebar*E);
+  double wS = p.Mag2() * sq(sin(p.Angle(q))) * (sq(F1) + QSqbar/(4.*mN*mN) * sq(kF2))/(E*Ebar);
+  double wI = -p.Mag()*sin(p.Angle(q))*(Ebar + E)*(sq(F1) + QSqbar/(4.*mN*mN) * sq(kF2))/(E*Ebar);
+  
   double sigmaMott = cmSqGeVSq * 4. * sq(alpha) * k.Mag2() * sq(cos(k.Theta()/2.)) / sq(QSq);
 
   double phi = q.Cross(k).Angle( q.Cross(p) );
