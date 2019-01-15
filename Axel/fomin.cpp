6c36b4885eafcbf66a430a74899395cfe3cbec72
Axel Schmidt <schmidta@mit.edu>
2019-01-15 16:08:08 -0500

    Fixed bug in which weight wasn't included in sparse tree.

--- Axel/fomin.cpp
+++ Axel/fomin.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
+#include <fstream>
 #include <cmath>
 #include <cstdlib>
 #include <cstdio>
 
+#include "TFile.h"
+#include "TTree.h"
+#include "TVector3.h"
+
 using namespace std;
 
+const double Ebeam=5.766;
+const double deg_margin=1.;
+
 int main(int argc, char ** argv)
-{
+{						
+  if (argc != 3)
+    {
+      cerr << "Wrong number of arguments. Instead use:\n"
+	   << "\tfomin /path/to/input /path/to/output\n\n";
+      return -1;
+    }
+
+  TFile * inF = new TFile(argv[1]);
+  TTree * inT = (TTree*) inF->Get("T");
+  const int nEvents = inT->GetEntries();
+
+
   cout << "Hello world.\n";
   return 0;
 }
--- gen_weight.cpp
+++ gen_weight.cpp
@@ -126,13 +126,13 @@ int main(int argc, char ** argv)
   outtree->Branch("pe",pe,"pe[3]/D");
   outtree->Branch("pLead",pLead,"pLead[3]/D");
   outtree->Branch("pRec",pRec,"pRec[3]/D");
+  outtree->Branch("weight",&weight,"weight/D");
   if (print_full_tree)
     {
       outtree->Branch("q",q,"q[3]/D");
       outtree->Branch("pMiss",pMiss,"pMiss[3]/D");
       outtree->Branch("pCM",pCM,"pCM[3]/D");
       outtree->Branch("pRel",pRel,"pRel[3]/D");
-      outtree->Branch("weight",&weight,"weight/D");
       outtree->Branch("QSq",&QSq,"QSq/D");
       outtree->Branch("xB",&xB,"xB/D");
       outtree->Branch("nu",&nu,"nu/D");
