b0fa418ed40edf63a2ae012ca58fa25dbf49ee63
Axel Schmidt <schmidta@mit.edu>
2019-01-15 15:47:28 -0500

    Added Axel directory for fomin-style analysis.

---
+++ Axel/fomin.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+  cout << "Hello world.\n";
+  return 0;
+}
