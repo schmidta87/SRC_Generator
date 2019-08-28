0fd8463a6469f76c73feb78f5d80e8c6b7abaf69
Jackson Pybus <Jrpybus@mit.edu>
2019-08-28 11:16:38 -0700

    Added code for HAND detector. Currently assuming constant 15 mrad angular smearing as a quick-and-dirty approximation.

--- include/detectors.h
+++ include/detectors.h
@@ -4,6 +4,7 @@
 #include "TVector3.h"
 
 bool HRS_hallA(TVector3 &v, double p_central, double phi_central);
-bool BigBite(TVector3 &v, double p_central, double phi_central);
+bool BigBite(TVector3 &v, double phi_central);
+bool HAND(TVector3 &v, double phi_central);
 
 #endif
--- simulator/detectors.cpp
+++ simulator/detectors.cpp
@@ -1,5 +1,7 @@
 #include "detectors.h"
 #include "TRandom3.h"
+#include "helpers.h"
+#include "constants.h"
 
 TRandom3 myRand(0);
 
@@ -8,55 +10,99 @@ bool HRS_hallA(TVector3 &v, double p_central, double phi_central)
 
   double theta_central = 0.5*TMath::Pi();
 
-  v.RotateY(0.5*TMath::Pi());
+  double mom_FWHM = 1e-4;
+  double h_FWHM = 0.5e-3;
+  double v_FWHM = 1e-3;
+  double mom_res = mom_FWHM/(2*sqrt(2*log(2)));
+  double h_res = h_FWHM/(2*sqrt(2*log(2)));
+  double v_res = v_FWHM/(2*sqrt(2*log(2)));
   
   double p = v.Mag();
   double theta = v.Theta();
   double phi = v.Phi();
 
-  if (abs(p/p_central - 1) < 0.045)
+  // Acceptance Region
+
+  if (abs(p/p_central - 1) > 4.5e-2)
     return false;
 
-  if (abs(phi - phi_central) < 0.030)
+  if (abs(phi - phi_central) > 30e-3)
     return false;
   
-  if (abs(theta - theta_central) < 0.060)
+  if (abs(theta - theta_central) > 60e-3)
     return false;
-  
-  p *= myRand.Gaus(1,1e-4/(2*sqrt(2*log(2))));
-  phi += myRand.Gaus(0,0.5e-3/(2*sqrt(2*log(2))));
-  theta += myRand.Gaus(0,1.0e-3/(2*sqrt(2*log(2))));
 
-  v.RotateY(-0.5*TMath::Pi());
+  // Resolution Smearing
+  
+  p *= (1. + mom_res*myRand.Gaus());
+  phi += h_res*myRand.Gaus();
+  theta += v_res*myRand.Gaus();
+
+  v.SetPtThetaPhi(p,theta,phi);
   
   return true;
 }
 
-bool BigBite(TVector3 &v, double p_central, double phi_central)
+bool BigBite(TVector3 &v, double phi_central)
 {
 
   double theta_central = 0.5*TMath::Pi();
 
-  v.RotateY(0.5*TMath::Pi());
-  
+  double mom_res = 1.5e-2;
+  double h_res = 7e-3;
+  double v_res = 13e-3;
+
   double p = v.Mag();
   double theta = v.Theta();
   double phi = v.Phi();
 
-  if (abs(p/p_central - 1) < 0.045)
-    return false;
+  // Acceptance Region
 
-  if (abs(phi - phi_central) < 0.030)
+  if (abs(phi - phi_central) > 80e-3)
     return false;
   
-  if (abs(theta - theta_central) < 0.060)
+  if (abs(theta - theta_central) > 300e-3)
+    return false;
+
+  // Resolution Smearing
+  
+  p *= (1. + mom_res*myRand.Gaus());
+  phi += h_res*myRand.Gaus();
+  theta += v_res*myRand.Gaus();
+  
+  v.SetPtThetaPhi(p,theta,phi);
+  
+  return true;
+}
+
+bool HAND(TVector3 &v, double phi_central)
+{
+
+  double theta_central = 0.5*TMath::Pi();
+  double d = 20.01; //ns
+
+  double p = v.Mag();
+  double theta = v.Theta();
+  double phi = v.Phi();
+
+  // Acceptance Region
+  
+  if (abs(phi - phi_central) > 80e-3)
     return false;
   
-  p *= myRand.Gaus(1,1.6e-3);
-  phi += myRand.Gaus(0,7e-3);
-  theta += myRand.Gaus(0,13e-3);
+  if (abs(theta - theta_central) > 300e-3)
+    return false;
 
-  v.RotateY(-0.5*TMath::Pi());
+  // Resolution Smearing
+  
+  double tof = d*sqrt(1.+sq(mN/p));
+  tof += 1.5*myRand.Gaus();
+  p = mN/sqrt(1-sq(tof/d));
+    
+  phi += 15e-3*myRand.Gaus();
+  theta += 15e-3*myRand.Gaus();
+  
+  v.SetPtThetaPhi(p,theta,phi);
   
   return true;
 }
