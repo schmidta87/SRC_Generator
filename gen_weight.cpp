d02729e0a8b6fb9449dae2652448cf74f4604498
jrpybus <jrpybus@eofe7.cm.cluster>
2019-11-13 11:32:56 -0500

    Fixed write buffer bug.

--- gen_weight.cpp
+++ gen_weight.cpp
@@ -264,6 +264,7 @@ int main(int argc, char ** argv)
   const double lambda_ei = alpha/M_PI * (log( 4.*Ebeam*Ebeam/(me*me)) - 1.);
   
   // Set up the tree
+  outfile->cd();
   TTree * outtree = new TTree("genT","Generator Tree");
   Double_t pe[3], q[3], pLead[3], pRec[3], pMiss[3], pCM[3], pRel[3];
   Double_t  QSq, xB, nu, pe_Mag, q_Mag, pLead_Mag, pRec_Mag, pMiss_Mag, pCM_Mag, pRel_Mag, theta_pmq, theta_prq, weight, lcweight;
@@ -495,7 +496,6 @@ int main(int argc, char ** argv)
     } 	  
 
   // Clean up
-  outfile->cd();
   outtree->Write();
   params.Write("parameters");
   outfile->Close();
